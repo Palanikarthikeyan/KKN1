8afed35886549ee4220649d6dc94a5126425e853
Palanikarthikeyan <abpalanikarthik@gmail.com>
2019-12-24 14:06:18 +0530

    Create p1.sh

---
+++ p1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Author: Mr.PalaniKarthikeyan
+#
+# Variable - identifier 
+# shell script support dynamic type structure 
+# How to declare and initialize variable in shell script?
+# Syntax:-
+# ---------
+# variablename=Value
+# 
+# variable name is user defined and casesensitive 
+#
+name="Karthik"
+dept="sales"
+code=001
+cost=12345.67
+ip="127.0.0.1"
+FNAME="/etc/passwd"
+echo "Name:$name
+Working department is:$dept
+Code number:$code
+File name:$FNAME
+IP Address:$ip"
+
+# end of the script
