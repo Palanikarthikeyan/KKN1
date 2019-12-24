3d26d635ae2dcbf5933cb0796d1645f28ab7bf14
Palanikarthikeyan <abpalanikarthik@gmail.com>
2019-12-24 14:28:21 +0530

    Create p2.sh

---
+++ p2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Author:Mr.Palani Karthikeyan
+# Write a shell script - declare and initialize employee details(name,working department,place,empid) to local variables
+# 
+
+name="Mr.Ram kumar" 
+dept=Sales
+EID="E123"
+place="Bangalore"
+
+echo "Employee name is:$name"
+echo "$name working department is $dept"
+echo "$name working place:$place"
+echo "EMP ID:$EID"
+
+# End of the line
